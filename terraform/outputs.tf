38ace591a93b7b142da36fbcd4dce601375fb4b4
Paul Yu <pauldotyu@gmail.com>
2021-07-01 22:40:44 -0700

    Update azure-pipeline.yml

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,12 +2,16 @@ output "rg_name" {
   value = azurerm_resource_group.grouper.name
 }
 
+output "acr_name" {
+  value = azurerm_container_registry.grouper.name
+}
+
 output "aks_name" {
   value = azurerm_kubernetes_cluster.grouper.name
 }
 
-output "acr_name" {
-  value = azurerm_container_registry.grouper.name
+output "akv_name" {
+  value = azurerm_key_vault.grouper.name
 }
 
 output "aks_managed_identity_client_id" {
